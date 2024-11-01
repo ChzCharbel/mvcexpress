ee430b5ddbf657bfc6fef567428bc61acc4cb8aa
ChzCharbel <charbel_chz@outlook.com>
2024-10-31 22:50:31 -0600

    FEAT: Add student routes

--- src/routes/student.ts
+++ src/routes/student.ts
@@ -5,11 +5,14 @@ import {
   createStudent,
   deleteStudent,
 } from "../controllers/student";
+import validate from "../middlewares/validate";
+import { studentSchema } from "../schemas/student";
 
 const router = Router();
 
 router.get("/", getStudents);
-router.post("/", createStudent);
+router.post("/", validate(studentSchema), createStudent);
+router.put("/:id", validate(studentSchema), updateStudent);
 router.delete("/:id", deleteStudent);
 
 router.put("/:id", updateStudent);
